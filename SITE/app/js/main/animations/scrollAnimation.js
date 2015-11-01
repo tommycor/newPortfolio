0090d0b0e8a39cd96be88fe54439409725d99c68
tommycor <tommycornilleau@gmail.com>
2015-11-02 00:45:36 +0100

    starting transitions js

--- SITE/app/js/main/animations/scrollAnimation.js
+++ SITE/app/js/main/animations/scrollAnimation.js
@@ -6,27 +6,59 @@
 var scrollAnimation = function( $window, ScrollManagerService ) {
 	var animation = {};
 
+	var transition = 1.5;
+
 	animation.enter = function (elem, done) {
 
 		// First Arrival
 		if ( typeof ScrollManagerService.direction === 'undefined' )
-			return;
-
-		// go up
-		if ( ScrollManagerService.direction === 'up' )
 		{
-			console.log('up')
-		}
-		// go down
-		else if ( ScrollManagerService.direction === 'down' )
-		{
-			console.log('down');
+			TweenMax.set(elem, {opacity: 0});
+			TweenMax.to(elem, 1, {delay:1, opacity: 1, onComplete: done});
 		}
 
 		var height = $window.innerHeight;
 		var width = $window.innerWidth;
 
-		// console.log(ScrollManagerService);
+		// go up
+		if ( ScrollManagerService.direction === 'up' )
+		{
+			TweenMax.set(elem, {y: height});
+			TweenMax.to(elem, transition / 2, {y:0, onComplete: done});
+		}
+		// go down
+		else if ( ScrollManagerService.direction === 'down' )
+		{
+			TweenMax.set(elem, {y: -height});
+			TweenMax.to(elem, transition / 2, {y:0, onComplete: done});
+		}
+	};
+
+	animation.leave = function (elem, done) {
+
+		// First Arrival
+		if ( typeof ScrollManagerService.direction === 'undefined' )
+		{
+			TweenMax.set(elem, {opacity: 0});
+			TweenMax.to(elem, 1, {delay:1, opacity: 1, onComplete: done});
+		}
+
+		var height = $window.innerHeight;
+		var width = $window.innerWidth;
+
+		// go up
+		if ( ScrollManagerService.direction === 'up' )
+		{
+			TweenMax.set(elem, {y: 0});
+			TweenMax.to(elem, transition / 2, {y: -height, onComplete: done});
+		}
+		// go down
+		else if ( ScrollManagerService.direction === 'down' )
+		{
+			TweenMax.set(elem, {y: 0});
+			TweenMax.to(elem, transition / 2, {y: height, onComplete: done});
+		}
+
 	};
 
 	return animation;	
